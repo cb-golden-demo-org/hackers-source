53f6a5813861b6140e7e5fa1c43f9188be4e91be
SK <skohulapalan@cloudbees.com>
2025-12-19 12:22:08 -0500

    feat: add version info and initialization logging to main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+// Hackers App v1.1.0 - Updated for enhanced performance
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -8,6 +9,8 @@ import store from './store'
 Vue.use(Buefy)
 
 Vue.config.productionTip = false
+
+console.log('Hackers App initializing...')
 
 new Vue({
   router,
